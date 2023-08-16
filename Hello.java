96fda691e8f50adcf2416a2c9af44daa6d44f3b9
Ja-Lo <142376663+lloydestrada@users.noreply.github.com>
2023-08-16 15:19:57 +0800

    Delete Hello.java

--- Hello.java
+++
@@ -1,6 +0,0 @@
-public class Hello {
-  public static void main(String[] args) {
-    System.out.println("Hello, Software Engineering 1!");
-    System.out.println("Student Name: ESTRADA, John Lloyd A.);
-  }
-}
